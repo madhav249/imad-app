6c463ca29a59b544a711ca762c7b1b18d8e07a63
madhav249 <30774713+madhav249@users.noreply.github.com>
2017-08-15 17:57:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,56 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne={
+    title: 'Article one || Madhava varma',
+    heading: 'Article-one',
+    date: '15th aug 2017',
+    content:'<p> The main reason why im intrestedin this course is only beacuse of practical kmowledge.</p>',
+    
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+var htmlTemplate=
+`<html>
+    <head>
+        <title>
+             ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+    <div class="container">   
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+          ${heading}
+        <div>
+            ${date}
+        </div>
+        ${content}
+        </div> 
+    </body>
+</html>
+
+`;
+return htmlTemplate;
+}
+
+
+
+
+
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send('createTemplate(articleOne)');
 });
 
 app.get('/article-one', function (req, res) {
