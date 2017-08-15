c10ab165e530db75aeeec6afe59c8439c4c67e83
madhav249 <30774713+madhav249@users.noreply.github.com>
2017-08-15 13:40:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('article one will be served.');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/articleone', function (req, res) {
-  res.sendFile('article one will be served.');
-});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
