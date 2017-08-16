2920d79aac9371ad07065d6ba667434106faf525
madhav249 <30774713+madhav249@users.noreply.github.com>
2017-08-16 21:31:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='hello';
+var img=document.getElementById('mad');
+img.onclick= function(){
+ img.style.marginLeft ='100px';   
+};
